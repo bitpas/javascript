bc78fce3a0fafde6cf1ec0088de9da1b7199039e
ryantoddgarza <ryantoddgarza@gmail.com>
2021-05-22 02:34:10 -0500

    feat: begin explicitly set import rules
    - static analysis
    - helpful warnings

--- configs/base/rules/imports.js
+++ configs/base/rules/imports.js
@@ -1,7 +1,6 @@
 const extensions = ['.js', '.json'];
 
 module.exports = {
-  extends: ['plugin:import/recommended'],
   plugins: ['import'],
   settings: {
     'import/extensions': extensions,
@@ -10,5 +9,26 @@ module.exports = {
         extensions,
       },
     },
+  },
+  rules: {
+    // Static analysis
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/default': 'off',
+    'import/namespace': 'error',
+    'import/no-self-import': 'error',
+    'import/no-cycle': 'error',
+
+    // Helpful warnings
+    'import/export': 'error',
+    'import/no-named-as-default': 'error',
+    'import/no-named-as-default-member': 'error',
+    'import/no-deprecated': 'off',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unused-modules': 'off',
+
+    // Style guide
+    'import/no-duplicates': 'error',
   },
 };
