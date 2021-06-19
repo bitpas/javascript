21dc6ed04d5dd114a6a5d618d0a3622cdcd5b221
ryantoddgarza <ryantoddgarza@gmail.com>
2021-06-19 16:13:18 -0500

    fix: detect react version

--- configs/react/rules/react.js
+++ configs/react/rules/react.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ['plugin:react/recommended'],
+  plugins: ['react'],
+  rules: {
+    'react/require-default-props': 'error',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
 };
